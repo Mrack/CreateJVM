2ae57fbdacaa6f37ce2b141204c3067c33e999cd
Mrack <Mrack@qq.com>
2021-08-20 09:17:59 +0800

    md5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include "Windows.h"
-#include "jni.h"
+#include <jni.h>
 
 
 static char *callJavaMd5(JNIEnv *env, char *pData, int size) {
@@ -22,6 +22,16 @@ static char *callJavaMd5(JNIEnv *env, char *pData, int size) {
     return reinterpret_cast<char *>(byte_array_elements);
 }
 
+
+char *toHex(char *input, int size) {
+    char *res = new char[size * 2 + 1]{0};
+    for (int i = 0; i < size; ++i) {
+        sprintf(res + i * 2, "%02X", (input[i] + 0xff) % 0xff);
+    }
+    return res;
+}
+
+
 int main() {
     std::string szJAVAHOME = getenv("JAVA_HOME");
     if (szJAVAHOME.empty()) {
@@ -38,13 +48,11 @@ int main() {
     vm_args.version = JNI_VERSION_1_6;
     vm_args.nOptions = 0;
     vm_args.ignoreUnrecognized = JNI_TRUE;
-    long status = JNI_CreateJavaVM(&jvm, (void **) &env, &vm_args);
 
+    long status = JNI_CreateJavaVM(&jvm, (void **) &env, &vm_args);
     if (status == JNI_OK) {
         char *result = callJavaMd5(env, "123", 3);
-        for (int i = 0; i < strlen(result); ++i) {
-            std::cout << std::hex << (int)result[i];
-        }
+        std::cout << toHex(result, 16) << std::endl;
     }
     return 0;
 }
